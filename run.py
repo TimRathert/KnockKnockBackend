fdcbd03a26a9f9a7a0c3f13362280fafcc2f538c
TimRathert <rathertt@gmail.com>
2022-10-10 10:56:28 -0400

    db seeded. HF interaction functional. working on reading db into hf
    removing api key

--- run.py
+++ run.py
@@ -1,16 +1,16 @@
 from flask import Flask, jsonify, request, g
 from flask_cors import CORS
-import pymongo, json
+import pymongo, json, requests, os
 from bson.json_util import dumps
-import os
 from dotenv import load_dotenv
-
+from sentence_transformers import SentenceTransformer
+import seed
 load_dotenv()
 
 db = os.environ.get("MONGODB_URI")
 
 DEBUG = True
-PORT = 4000
+PORT = os.environ.get("PORT")
 
 app = Flask(__name__)
 client = pymongo.MongoClient(db)
@@ -20,8 +20,15 @@ Database = client.get_database('knockknock')
 # Table
 jokes = Database.jokes
 
+
 def parse_json(data):
 	return json.loads(dumps(data))
+
+def query(payload):
+    API_URL = "https://api-inference.huggingface.co/models/sentence-transformers/all-MiniLM-L6-v2"
+    headers = ''
+    response = requests.post(API_URL, headers=headers, json=payload)
+    return response.json()
 
 # routes here 
 
@@ -30,9 +37,51 @@ def parse_json(data):
 def find():
 	thing = jokes.find({})
 	return parse_json(thing)
-	
+
+# seed stuff
+# Don't run this unless it's to reset the database lmao
+# @app.route('/seed', methods=['GET'])
+# def seeding():
+#     for joke in seed.jokes:
+#         def seedJokes(joke):
+#             queryObject = {
+#                 'user': 'Tim',
+#                 'setup': joke["name"],
+# 		        'punchline': joke["punch"]
+#             }
+#             query = jokes.insert_one(queryObject)
+#             return "Joke Added to Database"
+#         seedJokes(joke)
 
 
+# testing hugging face sentence transformer
+@app.route('/test', methods=['GET'])
+
+def dothething():
+    testVal = "who"
+    sents = [
+			"what",
+			"who",
+			"owl"
+		]
+    output = query({
+	"inputs": {
+		"source_sentence": testVal,
+		"sentences": sents
+	    },
+    })
+    mostSimilar = max(output)*100
+    if mostSimilar < 50:
+        return "I'm not sure about that..."
+    print(sents[output.index(mostSimilar)], mostSimilar)
+    return output
+
+# get setup from db test
+@app.route('/setup', methods=['GET'])
+
+def setup():
+    sents = jokes.find({},{"setup": 1, "_id":0})
+    return parse_json(sents.values())
 
 # Insert new joke
 @app.route('/insert-one/<setup>/<punchline>/', methods=['GET'])
